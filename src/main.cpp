109629c536b29d74763aa9b31e93e3a9ea15a2f0
netherfail04 <38269727+netherfail04@users.noreply.github.com>
2019-03-18 19:29:52 -0700

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    sf::CircleShape shape2(10.f);
+    shape.setFillColor(sf::Color::Green);
+    shape2.setFillColor(sf::Color::Blue);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.draw(shape2);
+        window.display();
+    }
+
+    return 0;
+}
